aa507c1f9f0b1854a43e09febb7a809341551faa
azyuzelev <azyuzelev@gmail.com>
2021-02-04 20:31:28 +0300

    zyuzelev 20:30 upd fix splash activity

--- app/src/main/java/com/example/films/SplashActivity.kt
+++ app/src/main/java/com/example/films/SplashActivity.kt
@@ -3,24 +3,49 @@ package com.example.films
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.airbnb.lottie.LottieAnimationView
+import com.google.gson.Gson
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.net.URL
 
 class SplashActivity : AppCompatActivity() {
     private var mHandler = Handler()
-    private lateinit var lottieAnimationView:LottieAnimationView
+    private lateinit var lottieAnimationView: LottieAnimationView
+    private lateinit var jsonFilms: Response
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
         lottieAnimationView = findViewById(R.id.lottie_splash)
-        Handler().postDelayed({
-            startActivity(
-                Intent(
-                    this@SplashActivity, MainActivity::class.java
+        downloadByUrl()
+    }
+
+    private fun downloadByUrl() {
+        GlobalScope.launch {
+            var response: String =
+                URL("https://api.themoviedb.org/3/movie/popular?api_key=d866b8cb9d02a5fc365da1327bc3f464&language=ru&page=1")
+                    .readText()
+            val gson = Gson()
+            jsonFilms = gson.fromJson(response, Response::class.java)
+            mHandler.postDelayed(parseIsReady, 0)
+        }
+    }
+
+    private val parseIsReady: Runnable = object : Runnable {
+        override fun run() {
+            if (jsonFilms.results?.size != 20) {
+                mHandler.postDelayed(this, 100)
+            } else {
+                val intent = Intent(baseContext, MainActivity::class.java)
+                intent.putExtra("response_list", jsonFilms)
+                startActivity(intent)
+                overridePendingTransition(
+                    R.transition.fade_in_transition,
+                    R.transition.fade_out_transition
                 )
-            )
-            finish()
-        }, 2500)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/films/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/films/ui/home/HomeFragment.kt
@@ -1,28 +1,25 @@
 package com.example.films.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.AnimationUtils
-import android.widget.Toast
+import android.view.animation.OvershootInterpolator
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.films.*
+import com.example.films.FilmAdapter
+import com.example.films.MainActivity
+import com.example.films.R
+import com.example.films.Response
 import com.example.films.ui.ClickListener.RecyclerItemClickListener
 import com.example.films.ui.FilmPage.FilmPageFragment
-import com.google.gson.Gson
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import java.net.URL
-
+import jp.wasabeef.recyclerview.adapters.SlideInRightAnimationAdapter
 
 class HomeFragment : Fragment() {
 
@@ -32,7 +29,6 @@ class HomeFragment : Fragment() {
     private lateinit var adapter: FilmAdapter
     private lateinit var homeViewModel: HomeViewModel
     private lateinit var jsonFilms: Response
-    private var mHandler = Handler()
     private lateinit var root: View
     private val fragment: Fragment = FilmPageFragment()
 
@@ -45,12 +41,13 @@ class HomeFragment : Fragment() {
             ViewModelProvider(this).get(HomeViewModel::class.java)
         root = inflater.inflate(R.layout.fragment_films, container, false)
         recyclerView = root.findViewById(R.id.recyclerView)
-        downloadByUrl()
+        jsonFilms = MainActivity().jsonFilms
+        adapter = FilmAdapter(jsonFilms)
+        createRecyclerView(adapter)
         return root
     }
 
     private fun createRecyclerView(adapter: FilmAdapter) {
-        layoutAnimation(recyclerView)
         linearLayoutManager =
             LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
         recyclerView.layoutManager = linearLayoutManager
@@ -58,6 +55,15 @@ class HomeFragment : Fragment() {
         gridLayoutManager = GridLayoutManager(context, 1)
         recyclerView.layoutManager = gridLayoutManager
         recyclerView.adapter = adapter
+        recyclerView.adapter = SlideInRightAnimationAdapter(adapter).apply {
+            // Change the durations.
+            setDuration(700)
+            // Change the interpolator.
+            setInterpolator(OvershootInterpolator())
+            // Disable the first scroll mode.
+            setFirstOnly(false)
+        }
+
 
         recyclerView.addOnItemTouchListener(
             RecyclerItemClickListener(
@@ -83,27 +89,27 @@ class HomeFragment : Fragment() {
         )
     }
 
-    private fun downloadByUrl() {
-        GlobalScope.launch {
-            var response: String =
-                URL("https://api.themoviedb.org/3/movie/now_playing?api_key=d866b8cb9d02a5fc365da1327bc3f464&language=ru&page=1")
-                    .readText()
-            val gson = Gson()
-            jsonFilms = gson.fromJson(response, Response::class.java)
-            mHandler.postDelayed(parseIsReady, 0)
-        }
-    }
+    /* private fun downloadByUrl() {
+         GlobalScope.launch {
+             var response: String =
+                 URL("https://api.themoviedb.org/3/movie/popular?api_key=d866b8cb9d02a5fc365da1327bc3f464&language=En-US&page=1")
+                     .readText()
+             val gson = Gson()
+             jsonFilms = gson.fromJson(response, Response::class.java)
+             mHandler.postDelayed(parseIsReady, 0)
+         }
+     }*/
 
-    private fun layoutAnimation(recyclerView: RecyclerView) {
+    /*private fun layoutAnimation(recyclerView: RecyclerView) {
         var context = recyclerView.context
         var layoutAnimationController =
             AnimationUtils.loadLayoutAnimation(context, R.anim.layout_slide_right)
         recyclerView.layoutAnimation = layoutAnimationController
         recyclerView.adapter?.notifyDataSetChanged()
         recyclerView.scheduleLayoutAnimation()
-    }
+    }*/
 
-    private val parseIsReady: Runnable = object : Runnable {
+    /*private val parseIsReady: Runnable = object : Runnable {
         override fun run() {
             if (jsonFilms.results?.size!! < 20) {
                 mHandler.postDelayed(this, 100)
@@ -112,7 +118,7 @@ class HomeFragment : Fragment() {
                 createRecyclerView(adapter)
             }
         }
-    }
+    }*/
 
     override fun onDestroyView() {
         super.onDestroyView()
